944c9a03a3d50e4d9d730ce0adaff1ed292fd845
ancilts <147413580+ancilts@users.noreply.github.com>
2024-01-20 06:24:37 +0000

    commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+app.route('/prediction')
+def predict():
+    return 'Ancil'
+
+if __name__ == '__main__':
+    app.run()
